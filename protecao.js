231e4c99662a3fa97babe93c995ba8a1b27aefe1
tvgratisonline12 <tvgratisonline12@gmail.com>
2026-07-01 11:35:53 -0300

    Update and rename protecao.js to player.js

--- protecao.js
+++
@@ -1,7 +0,0 @@
-
-(function() {
-    var dominioOficial = "tvgratis.online";
-    if (window.location.hostname !== dominioOficial) {
-        window.location.replace("https://" + dominioOficial);
-    }
-})();
