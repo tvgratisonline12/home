aa2020afb0641f0a74b9c0cfc031be7e8f8b5ede
tvgratisonline12 <tvgratisonline12@gmail.com>
2026-07-01 10:30:04 -0300

    Create protecao.js

---
+++ protecao.js
@@ -0,0 +1,7 @@
+
+(function() {
+    var dominioOficial = "tvgratis.online";
+    if (window.location.hostname !== dominioOficial) {
+        window.location.replace("https://" + dominioOficial);
+    }
+})();
