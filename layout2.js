79046d6b41ee8eacd45a4733eef858d21bcaed5e
tvgratisonline12 <tvgratisonline12@gmail.com>
2026-07-03 19:01:25 -0300

    Update layout2.js

--- layout2.js
+++ layout2.js
@@ -7,7 +7,7 @@ let fadeTimeout;
 
 async function carregarCanaisJSON() {
     try {
-        const response = await fetch('https://tvgratis.online/45s84e1sfer.json');
+        const response = await fetch('https://tvgratis.online/45s84e1free.json');
         if (!response.ok) throw new Error('Falha');
         canaisRaw = await response.json();
         const s = new Set(['Todos']);
