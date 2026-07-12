c71db6142aba3e47809924154ecd1cecf84dfca1
tvgratisonline12 <tvgratisonline12@gmail.com>
2026-07-11 22:19:23 -0300

    Update layout2.js

--- layout2.js
+++ layout2.js
@@ -146,23 +146,19 @@ if (qual === "rd") {
 // --- 5. RENDERIZAÇÃO ---
 container.innerHTML = ""; // Limpa o player antes de renderizar
 
-} else if (qual === 'pl') {
+if (qual === 'pl') {
     // Tela de aviso para canais que precisam de link externo
     container.innerHTML = `
-        <div id="aviso-pl" style="width:100%; height:100%; display:flex; flex-direction:column; align-items:center; justify-content:center; background:linear-gradient(135deg, #1a1a1a 0%, #000 100%); color:white; text-align:center; padding:20px; box-sizing:border-box;">
-            
-            <div id="welcome-title" style="margin-bottom: 15px; font-size: 1.5rem;">Aviso Importante</div>
-            
-            <div id="welcome-sub" style="margin-bottom: 25px; line-height: 1.4; max-width: 90%;">
-                Este canal precisa ser aberto em um link<br>externo para funcionar corretamente.
-            </div>
+         <div id="aviso-pl" style="width:100%; height:100%; display:flex; flex-direction:column; align-items:center; justify-content:center; background:linear-gradient(135deg, #1a1a1a 0%, #000 100%); color:white; text-align:center; padding:20px; box-sizing:border-box;">
+            <div id="welcome-title">Aviso Importante</div>
+            <div id="welcome-sub" style="animation: fadeIn 1s forwards 0.5s">Este canal Precisa Ser Aberto em um link.<br>
+                Externo para Funcionar Corretamente</div> <br><br>
         
-            <a href="${urlVideo}" target="_blank" style="padding: 15px 40px; background: #a855f7; color: white; text-decoration: none; border-radius: 8px; font-weight: bold; font-family: sans-serif; transition: 0.3s; cursor: pointer; display: inline-block; text-align: center;">
+            <a href="${urlVideo}" target="_blank" style="padding: 15px 30px; background: #a855f7; color: white; text-decoration: none; border-radius: 8px; font-weight: bold; font-family: sans-serif; transition: 0.3s; cursor: pointer;">
                 ABRIR CANAL
             </a>
-        </div>
+    
     `;
-}
 } else if (['sd', 'ec'].includes(qual)) {
     // Video.js para streamings diretos
     container.innerHTML = `<video id="video-player" class="video-js vjs-big-play-centered" controls autoplay playsinline style="width:100%;height:100%;"></video>`;
