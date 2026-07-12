361a603df4867aed0259d250fef561884ca58573
tvgratisonline12 <tvgratisonline12@gmail.com>
2026-07-12 10:13:32 -0300

    Update layout2.js

--- layout2.js
+++ layout2.js
@@ -81,10 +81,28 @@ function renderList() {
         if (['rd', 'ad'].includes(qual)) {
             badgeHtml = '<span class="ad-badge" style="background-color:red; color:white; padding:0 4px; border-radius:3px; margin-left:5px; font-size:10px; font-weight:bold;">AD</span>';
         }
-        
+        let plIcon = '';
+
+if (qual === 'pl') {
+    plIcon = `
+    <svg xmlns="http://www.w3.org/2000/svg"
+         width="14"
+         height="14"
+         viewBox="0 0 24 24"
+         fill="none"
+         stroke="#2196F3"
+         stroke-width="2.2"
+         stroke-linecap="round"
+         stroke-linejoin="round"
+         style="vertical-align:middle;margin-right:6px;">
+        <path d="M14 3h7v7"/>
+        <path d="M10 14L21 3"/>
+        <path d="M21 14v7H3V3h7"/>
+    </svg>`;
+}
         div.innerHTML = `
             <span class="channel-number">${(idx + 1).toString().padStart(2, '0')}</span>
-            <span>${item.canal || "Canal"}</span>
+            <span>${plIcon}${item.canal || "Canal"}</span>
             ${badgeHtml}
         `;
         
